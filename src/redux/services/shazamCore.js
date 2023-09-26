f0d09faa1a4c36e2ef60d26125783d1ecbe61a57
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2023-09-26 23:46:06 +1000

    discoveries 2

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,14 +3,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-api7.p.rapidapi.com',
+        baseUrl: 'https://shazam.p.rapidapi.com',
         prepareHeaders: () => {
             headers.set('X-RapidAPI-Key', '87062783ffmsh1984b1a9dc2d616p11256cjsn35f8ecc6a6e9');
             return headers;
         },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/charts/get-top-songs-in-world' }),
+        getTopCharts: builder.query({ query: () => '/charts/list' }),
     }),
 });
 
